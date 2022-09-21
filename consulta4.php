c51d3a405897d020e265a1b11de35d8db2938cb2
Anuar Moreno <anuarmoreno09@gmail.com>
2022-09-20 20:51:27 -0500

    cambios en el yml credenciales para github

--- consulta4.php
+++ consulta4.php
@@ -22,25 +22,6 @@ if (isset($_GET["var1"]) && isset($_GET["var2"]) && isset($_GET["var3"]) && isse
     $maxlon2 = $_GET["var3"];
     $minlon2 = $_GET["var4"];
 
-
-            require_once 'vendor/autoload.php';
-
-        $dotenv = Dotenv\Dotenv::createImmutable('varen/');
-        $dotenv->load();
-
-        $host=$_ENV['RDS_HOST'];
-        $usuario=$_ENV['RDS_USER'];
-        $contraseña=$_ENV['RDS_PASSWORD'];
-        $base=$_ENV['RDS_DATABASE'];
-
-        $conexion= new mysqli($host, $usuario, $contraseña, $base);
-        if ($conexion -> connect_errno)
-        {
-                die("Fallo la conexion:(".$conexion -> mysqli_connect_errno().")".$conexion->
-                        mysqli_connect_error());
-        }
-
-       // include('consulta.php');
     
 
     $sql = mysqli_query($conexion, "SELECT envio FROM registro_posicion  WHERE latitud >= '$minlat2' AND latitud <= '$maxlat2' AND longitud >= '$minlon2' AND longitud <= '$maxlon2' ");
