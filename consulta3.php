3aa0152be4e38f9a2e8020fac94e6b81a876f16a
Anuar Moreno <anuarmoreno09@gmail.com>
2022-09-20 22:31:44 -0500

    cambios en el yml credenciales para github

--- consulta3.php
+++ consulta3.php
@@ -1,4 +1,4 @@
-<?php
+<<?php
 require_once 'vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable('varen/');
@@ -23,14 +23,35 @@ if (isset($_GET["var1"]) && isset($_GET["var2"])) {
 
     $str = $_GET["var1"];
     $fnsh = $_GET["var2"];
+
+    $sql = mysqli_query($conexion, "SELECT latitud, longitud FROM registro_posicion  WHERE envio >= '$str' AND envio <= '$fnsh' ");
+
+    $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+    exit(json_encode($result));
+
 }
 
-$sql = mysqli_query($conexion, "SELECT latitud, longitud FROM registro_posicion  WHERE envio >= '$str' AND envio <= '$fnsh' ");
 
-$result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+if (isset($_GET["var3"]) && isset($_GET["var4"]) && isset($_GET["var5"]) && isset($_GET["var6"])) {
 
-exit(json_encode($result));
+    $maxlat2 = $_GET["var3"];
+    $minlat2 = $_GET["var4"];
+    $maxlon2 = $_GET["var5"];
+    $minlon2 = $_GET["var6"];
 
+
+
+    $sql = mysqli_query($conexion, "SELECT envio FROM registro_posicion  WHERE latitud >= '$minlat2' AND latitud <= '$maxlat2' AND longitud >= '$minlon2' AND longitud <= '$maxlon2' ");
+
+    $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+    
+    exit(json_encode($result));
+
+
+
+
+}
 
 
 
