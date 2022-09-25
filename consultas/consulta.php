5141fab51d8fd97c81455d7c10b5b53d4a8e31f6
Anuar Moreno <anuarmoreno09@gmail.com>
2022-09-25 14:37:51 -0500

    actualizando archivos divididos

--- consultas/consulta.php
+++ consultas/consulta.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once 'var/www/html/vendor/autoload.php';
+require_once '/var/www/html/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable('var/www/html/varen/');
+$dotenv = Dotenv\Dotenv::createImmutable('/var/www/html/varen/');
 $dotenv->load();
 
 $host=$_ENV['RDS_HOST'];
