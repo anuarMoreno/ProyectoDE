8b40a484eaff75cac7b587ee1442577ab9bec41e
Anuar Moreno <anuarmoreno09@gmail.com>
2022-09-28 08:34:31 -0500

    actualizando archivos divididos

--- consultas/consulta4.php
+++ consultas/consulta4.php
@@ -23,14 +23,8 @@ if (isset($_GET["var1"]) && isset($_GET["var2"]) && isset($_GET["var3"])) {
     $sendlon2 = $_GET["var2"];
     $radius = $_GET["var3"];
 
-<<<<<<< HEAD
+
     
-
-    $sql = mysqli_query($conexion, "SELECT envio FROM registro_posicion  WHERE latitud >= '$minlat2' AND latitud <= '$maxlat2' AND longitud >= '$minlon2' AND longitud <= '$maxlon2' ");
-=======
-
-    $conn = mysqli_connect('ddatabase.ct8hfoibhvoa.us-east-1.rds.amazonaws.com', 'admin', 'Meteoritos21', 'davidserver');
->>>>>>> develop
 
     $sql = mysqli_query($conn, "SELECT envio, ( 6371 * acos( cos( radians($sendlat2) ) * cos( radians( latitud ) ) 
     * cos( radians( longitud ) - radians($sendlon2) ) + sin( radians($sendlat2) ) * sin(radians(latitud)) ) ) AS distance 
